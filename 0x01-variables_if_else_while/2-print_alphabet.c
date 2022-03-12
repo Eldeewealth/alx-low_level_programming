22004391d5000d1adad8b059da9f3247458185f2
eldeewealth <lovedayokoro93@gmail.com>
2022-03-12 10:38:03 -0800

    the source code in order to print the last digit of the number stored in the variable n

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,8 +4,8 @@
  * main - prints the alphabet in lowercase,
  * followed by a new line
  * Return: Always 0 (Success)
- *  
- * function declaration
+ *
+ * function main declaration
  */
 int main(void)
 {
