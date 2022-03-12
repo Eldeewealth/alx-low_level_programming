8e059f94854189a52ef91ce7ddab002efddf6812
eldeewealth <lovedayokoro93@gmail.com>
2022-03-12 10:29:59 -0800

    the source code in order to print the last digit of the number stored in the variable n

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 /*
- * main - Prints the alphabet.main
- *
- * main - Prints numbers between 00 to 89
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+		char ch;
 
-	for (i = 0; i < 26; i++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 	{
-	putchar(alp[i]);
+		putchar(ch);
 	}
-	putchar('\n');
-	return (0);
+		putchar('\n');
+		return (0);
 }
