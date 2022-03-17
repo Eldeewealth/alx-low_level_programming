22f3931a22c033c9e1d67eea59efb5a110f26832
eldeewealth <lovedayokoro93@gmail.com>
2022-03-17 02:34:27 -0700

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,19 @@
 #include ="main"
 
 /**
+ *print_alphabet - a function that prints the alphabet
  *
- *
- *
+ *Description: a function that prints the alphabet,
+ * in lowercase, followed by a new line.
  */
 
-void print_alphabet_x10(void) 
-	{	
-	
+void print_alphabet_x10(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+		{
+	_putchar(i);
+			}
+	_putchar('\n');
+
 	}
