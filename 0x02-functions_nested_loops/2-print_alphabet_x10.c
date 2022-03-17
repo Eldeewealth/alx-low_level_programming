bc0cffea9527b84e1ee9f54c0739f92d71485696
eldeewealth <lovedayokoro93@gmail.com>
2022-03-17 04:52:00 -0700

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,9 +12,9 @@ void print_alphabet_x10(void)
 	int a,b;
 for (a = 0; a <= 9; a++)
 {
-	for (b = 'a'; i <= 'z'; i++)
+	for (b = 'a'; b <= 'z'; i++)
 		{
-	_putchar(i);
+	_putchar(b);
 			}
 	_putchar('\n');
 	}
