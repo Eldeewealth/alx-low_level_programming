5719242e0eef7e08cf34aed79361b7d0501b86cc
eldeewealth <lovedayokoro93@gmail.com>
2022-03-17 09:43:15 -0700

    new update

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -13,9 +13,9 @@ int main(void)
 	sum = 0;
 		for (i = 3; i < 1024; i++)
 			{
-		if ((i % 3 == 0) || (i % 5 == 0))
+	if ((i % 3 == 0) || (i % 5 == 0))
 			{
-				sum += i;
+			sum += i;
 		}
 			}
 			printf("%d\n", sum);
