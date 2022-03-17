1f08e0f9c55afdc538454646373610235d452045
eldeewealth <lovedayokoro93@gmail.com>
2022-03-17 04:25:27 -0700

    update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
  * _isalpha  - A function that checks for alphabet.
  * @c: the character to be checked
  * Return: 1 if c is alpha, 0 otherwise
-   */
+ */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
