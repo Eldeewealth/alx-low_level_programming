eae78cb26275ab7409d7e15b47e311321bc931ba
eldeewealth <lovedayokoro93@gmail.com>
2022-03-18 08:53:52 -0700

    print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,21 +7,13 @@
 
 void more_numbers(void)
 {
-	int i, j;
-	char c;
+	int i, b;
 
 	for (i = 0; i < 10; i++)
+	{
+	for (b = '0'; b < '15'; b++)
 		{
-		c = '0';
-	for (j = 0; j <= 14; j++)
-		{
-		if (j > 9)
-		_putchar('1');
-		_putchar(c);
-		if (c == '9')
-		c = '0';
-		else
-		c++;
+			_putchar(b);
 		}
 		_putchar('\n');
 	}
