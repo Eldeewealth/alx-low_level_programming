171e2b92d70d44d6c551d9a45e5b94e5fd465a43
eldeewealth <lovedayokoro93@gmail.com>
2022-03-18 06:40:27 -0700

    check digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - a function that checks for uppercase character.
+ * @c: the character to check
+ * Return: 1 for uppercase characters, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c <= '9')
+	{
+	return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
