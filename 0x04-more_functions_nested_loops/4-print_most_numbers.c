551fde138b26986c1d92446bea99353b6c67f2de
eldeewealth <lovedayokoro93@gmail.com>
2022-03-18 08:19:39 -0700

    print most numbers added

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - A function that prints
+ *print_most_numbers - A function that prints
  * the numbers, from 0 to 9, except  2 and 4
  */
 
@@ -13,6 +13,6 @@ void print_most_numbers(void)
 	{
 		if(i != '2' && i != '4')
 		_putchar(i);
-				}
+	}
 		_putchar('\n');
 }
