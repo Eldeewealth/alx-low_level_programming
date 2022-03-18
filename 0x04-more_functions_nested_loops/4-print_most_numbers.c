9742b5ce38b7b80b529b9534fd3e1d8f29d660c9
eldeewealth <lovedayokoro93@gmail.com>
2022-03-18 08:23:35 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,14 +4,13 @@
  *print_most_numbers - A function that prints
  * the numbers, from 0 to 9, except  2 and 4
  */
-
 void print_most_numbers(void)
 {
 	int i;
 
 	for (i = '0'; i < '9'; i++)
 	{
-		if(i != '2' && i != '4')
+		if(i != 50 && i != 52)
 		_putchar(i);
 	}
 		_putchar('\n');
