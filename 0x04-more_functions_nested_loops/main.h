631f2dc82ba50458ab651b009839fc5c9f0a4685
eldeewealth <lovedayokoro93@gmail.com>
2022-03-19 05:15:08 -0700

    updated main.h updated

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef _main_h_
+#define _main_h_
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -7,3 +10,5 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+
+#endif
