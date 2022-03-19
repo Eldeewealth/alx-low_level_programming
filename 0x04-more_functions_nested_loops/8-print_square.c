23d637550f693093ec3ae5b55eb1915a9d8d1780
eldeewealth <lovedayokoro93@gmail.com>
2022-03-19 16:23:53 -0700

    update to main and 8print square script

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - a function that prints a square
+ * followed by a new line.
+ *
+ * @size: size of the square.
+ * Return: no return.
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+				{
+				_putchar('#');
+				}
+			_putchar('\n');
+		}
+	}
+}
