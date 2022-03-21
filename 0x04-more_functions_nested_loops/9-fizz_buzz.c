bb846f65c321f6b815d72529764d1ef9ab92613a
eldeewealth <lovedayokoro93@gmail.com>
2022-03-20 22:30:14 -0700

    Fuzz buzz updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,6 +8,8 @@ int main(void)
 {
 	int n;
 
+	n = 1;
+	printf("%d", n);
 	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
@@ -26,8 +28,6 @@ int main(void)
 		{
 		printf(" %d", n);
 		}
-		if (i < 100)
-		printf(" ");
 	}
 	printf("\n");
 	return (0);
