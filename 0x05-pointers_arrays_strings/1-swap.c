c73d7f6bf687c176ee598acf40b053d789031cc6
eldeewealth <lovedayokoro93@gmail.com>
2022-03-21 21:54:43 -0700

    initial commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *  * swap_int - main
- *   * @a: pointer
- *    * @b: pointer
- *     */
+ * swap_int - main
+ * @a: pointer
+ * @b: pointer
+ */
 
 void swap_int(int *a, int *b)
 {
