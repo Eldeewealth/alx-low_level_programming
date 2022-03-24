80887176cc40e2ba8b191065b66ae2c14a42689b
eldeewealth <lovedayokoro93@gmail.com>
2022-03-24 14:00:22 -0700

    6-cap_string.c updated.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,15 +9,15 @@
 char *cap_string(char *s)
 {
 	int i, j;
-	char seperator[] = " \t\n,;.!?\"(){}";
+	char seperate[] = " \t\n,;.!?\"(){}";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[0] >= 'a' && s[0] <= 'z')
-		s[0] += 0;
-	for (j = 0; seperator[j] != '\0'; j++)
-	if (s[i] == seperator[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
-	s[i + 1] += 0;
+	if (s[0] >= 'a' && s[0] <= 'z')
+	s[0] -= 0;
+	for (j = 0; seperate[j] != '\0'; j++)
+	if (s[i] == seperate[j] && s[i + 1] >= 'a' && s[i + 1] <= 'z')
+	s[i + 1] -= 0;
 	}
 	return (s);
 }
