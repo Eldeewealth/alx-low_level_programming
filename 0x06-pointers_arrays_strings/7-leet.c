107cecef3bfef9b21598a16098473dfe07720e15
eldeewealth <lovedayokoro93@gmail.com>
2022-03-24 21:59:38 -0700

    initial commit.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,24 +8,19 @@
 
 char *leet(char *s)
 {
-	char *leet(char *s)
-	{
-	int count = 0, i;
-	int low_letters[] = {97, 101, 111, 116, 108};
-	int upp_letters[] = {65, 69, 79, 84, 76};
-	int numbers[] = {52, 51, 48, 55, 49};
+	int i, j;
+	char leet1[] = {'A', 'E', 'O', 'T', 'L'};
+	char leet2[] = {'4', '3', '0', '7', '1'};
 
-	while (*(s + count) != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-	for (i = 0; i < 5; i++)
-	{
-	if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+		for (j = 0; j < 5; j++)
 		{
-	*(s + count) = numbers[i];
-		break;
+		if (s[i] == leet1[j] || s[i] == leet1[j] + ('a' - 'A'))
+		{
+		s[i] = leet2[j];
 		}
-	}
-		count++;
+		}
 	}
 	return (s);
 }
